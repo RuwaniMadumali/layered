3ac872925dfa0c8d6d3c3a9e93f0b1878a01a244
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-28 15:11:23 +0530

    Stage2: Revamping DAOS

--- src/dao/ItemDAO.java
+++ src/dao/ItemDAO.java
@@ -1,4 +1,28 @@
 package dao;
 
+import entity.Item;
+
+import java.util.List;
+
 public class ItemDAO {
+
+    public static List<Item> findAllItems() {
+
+    }
+
+    public static Item findItem(String itemCode) {
+
+    }
+
+    public static boolean saveItem(Item Item) {
+
+    }
+
+    public static boolean updateItem(Item Item) {
+
+    }
+
+    public static boolean deleteItem(String itemCode) {
+
+    }
 }
